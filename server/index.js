24934af487b1f0be852ecdf7b479de63ae0eac0a
RuslanGon <ruslan441006@gmail.com>
2025-07-18 13:03:38 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import startServer from './db.js';
 import registerValidator from './validations/auth.js';
-import { validatorResult } from 'express-validator';
+import { validationResult } from 'express-validator';
 
 
 const app = express()
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.post("/auth/register", registerValidator, (req, res) => {
-  const errors = validatorResult(req);
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
