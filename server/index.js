f474edf7a88f0d18f226f1c7cf7fb6692986329a
RuslanGon <ruslan441006@gmail.com>
2025-07-18 12:07:27 +0300

    wre

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
-
+import cors from 'cors'
 import startServer from './db.js';
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
 
 startServer(app);
