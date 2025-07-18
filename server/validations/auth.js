df11e161664e7efe1d2803f003f888608e89b481
RuslanGon <ruslan441006@gmail.com>
2025-07-18 12:52:01 +0300

    wer

--- server/validations/auth.js
+++ server/validations/auth.js
@@ -6,12 +6,17 @@ const registerValidator = [
     .withMessage('Email должен быть валидным адресом'),
 
   body('password')
-    .isLength({ min: 2 })  // обычно минимум 6 символов для пароля
+    .isLength({ min: 2 }) 
     .withMessage('Пароль должен содержать минимум 2 символов'),
 
   body('fullName')
     .isLength({ min: 3 })
     .withMessage('Имя должно содержать минимум 3 символа'),
+
+    body('avatarUrl')
+    .optional()
+    .isURL()
+    .withMessage('Аватар должен быть валидным URL')
 ];
 
 export default registerValidator;
