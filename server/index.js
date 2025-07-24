25eb365b9567ad85d34c479db226da63f8f24e48
RuslanGon <ruslan441006@gmail.com>
2025-07-24 11:07:20 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -56,32 +56,36 @@ app.post("/auth/register", registerValidator, async (req, res) => {
 });
 
 app.post('/auth/login', async (req, res) => {
-try {
-  const user = await UserModel.findOne({email:req.body.email})
-  if(!user) {
-    return res.status(404).json({message:'Пользователь не найдет'});
-  }
-  const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-  if(isValidPass) {
-    return res.status(404).json({message:'Неверный логин или пароль'});
-  }
-  const token = jwt.sign(
-    { _id: user._id },
-    process.env.SECRET_KEY,
-    { expiresIn: '30d' }
-  );
-  const { passwordHash: _, ...userData } = user._doc;
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
+    }
 
-  res.json({
-    ...userData,
-    token,
-  });
-} catch (error) {
-  console.error(err);
-  res.status(500).json({
-    message: 'Не удалось авторизоваться',
-  });
-}
-})
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+    if (!isValidPass) {
+      return res.status(400).json({ message: 'Неверный логин или пароль' });
+    }
+
+    const token = jwt.sign(
+      { _id: user._id },
+      process.env.SECRET_KEY,
+      { expiresIn: '30d' }
+    );
+
+    const { passwordHash: _, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: 'Не удалось авторизоваться',
+    });
+  }
+});
 
 startServer(app);
