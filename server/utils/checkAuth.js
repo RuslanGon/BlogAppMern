fba929fd73885b539705f20b5ef8b3d03c11d369
RuslanGon <ruslan441006@gmail.com>
2025-07-24 11:52:49 +0300

    wer

--- server/utils/checkAuth.js
+++ server/utils/checkAuth.js
@@ -1,21 +1,17 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const checkAuth = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
-  // Проверяем наличие заголовка и Bearer
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Нет доступа (токен не найден)' });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Нет доступа (токен не найден)" });
   }
-
-  const token = authHeader.split(' ')[1];
-
+  const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req.userId = decoded._id; // Сохраняем ID пользователя
+    req.userId = decoded._id;
     next();
   } catch (err) {
     console.error(err);
-    return res.status(403).json({ message: 'Нет доступа (токен невалиден)' });
+    return res.status(403).json({ message: "Нет доступа (токен невалиден)" });
   }
 };
