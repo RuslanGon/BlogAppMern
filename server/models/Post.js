f923816085f91c1a00f59d6ad4a6cbd6513664ac
RuslanGon <ruslan441006@gmail.com>
2025-07-24 12:34:26 +0300

    wer

---
+++ server/models/Post.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    tags: { type: Array, default: [] },
+    viewsCount: {type: Number, default: 0},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true  },
+    imageUrl: String
+  },
+  { timestamps: true }
+);
+
+const PostModel = mongoose.model("Post", postSchema);
+
+export default PostModel;
