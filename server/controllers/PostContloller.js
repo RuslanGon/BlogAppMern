d9e363579acd0cd9f030d387bbf9bb824027ed20
RuslanGon <ruslan441006@gmail.com>
2025-07-24 12:42:04 +0300

    wre

--- server/controllers/PostContloller.js
+++ server/controllers/PostContloller.js
@@ -1,7 +1,22 @@
-export const createPost = async(req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+import PostModel from "../models/Post.js";
+
+export const createPost = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      tags: req.body.tags,
+      user: req.userId,
+      imageUrl: req.body.imageUrl,
+    });
+
+    const post = await doc.save();
+
+    res.status(201).json(post);
+  } catch (error) {
+    console.error('Ошибка при создании поста:', error);
+    res.status(500).json({
+      message: 'Не удалось создать пост',
+    });
+  }
+};
