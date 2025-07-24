32f563b872e2707c40479c5a3ac9cd17d5f6ccd3
RuslanGon <ruslan441006@gmail.com>
2025-07-24 13:24:28 +0300

    wer

--- server/controllers/PostContloller.js
+++ server/controllers/PostContloller.js
@@ -33,7 +33,22 @@ export const getPostById = async (req, res) => {
   }
 };
 
-
+export const deletePost = async (req, res) => {
+    try {
+      const postId = req.params.id;
+  
+      const deletedPost = await PostModel.findByIdAndDelete(postId);
+  
+      if (!deletedPost) {
+        return res.status(404).json({ message: "Пост не найден" });
+      }
+  
+      res.json({ message: "Пост успешно удалён" });
+    } catch (error) {
+      console.error("Ошибка при удалении поста:", error);
+      res.status(500).json({ message: "Не удалось удалить пост" });
+    }
+  };
 
 export const createPost = async (req, res) => {
   try {
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import startServer from './db.js';
 import registerValidator from './validations/auth.js';
 import {checkAuth} from './utils/checkAuth.js'
 import { getMe, login, register } from './controllers/UserController.js';
-import { createPost, getAllPosts, getPostById } from './controllers/PostContloller.js';
+import { createPost, deletePost, getAllPosts, getPostById } from './controllers/PostContloller.js';
 import postValidator from './validations/post.js';
 
 const app = express();
@@ -25,5 +25,9 @@ app.post('/posts', checkAuth, postValidator, createPost)
 app.get('/posts', getAllPosts)
 
 app.get('/posts/:id', getPostById)
+
+app.delete('/posts/:id', deletePost )
+
+
 
 startServer(app);
