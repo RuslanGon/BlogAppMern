4341fa19168dff2ed13259cf1dba80120c3e03b2
RuslanGon <ruslan441006@gmail.com>
2025-07-24 10:58:00 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -55,4 +55,33 @@ app.post("/auth/register", registerValidator, async (req, res) => {
   }
 });
 
+app.post('/auth/login', async (req, res) => {
+try {
+  const user = await UserModel.findOne({email:req.body.email})
+  if(!user) {
+    return res.status(404).json({message:'Пользователь не найдет'});
+  }
+  const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+  if(isValidPass) {
+    return res.status(404).json({message:'Неверный логин или пароль'});
+  }
+  const token = jwt.sign(
+    { _id: user._id },
+    process.env.SECRET_KEY,
+    { expiresIn: '30d' }
+  );
+  const { passwordHash: _, ...userData } = user._doc;
+
+  res.json({
+    ...userData,
+    token,
+  });
+} catch (error) {
+  console.error(err);
+  res.status(500).json({
+    message: 'Не удалось авторизоваться',
+  });
+}
+})
+
 startServer(app);
