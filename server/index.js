4a3fbdb0eea6b02a9b035a8798b9c30ba39ce7dd
RuslanGon <ruslan441006@gmail.com>
2025-07-28 18:42:18 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -6,8 +6,39 @@ import {checkAuth} from './utils/checkAuth.js'
 import { getMe, login, register } from './controllers/UserController.js';
 import { createPost, deletePost, getAllPosts, getPostById, updatePost } from './controllers/PostContloller.js';
 import postValidator from './validations/post.js';
+import multer from 'multer'
 
 const app = express();
+
+// Multer(loading image)
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+      cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    },
+  });
+  
+const upload = multer({ storage });
+
+// Загрузка изображений
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    try {
+      res.json({
+        url: `/uploads/${req.file.filename}`,
+      });
+    } catch (err) {
+      console.error('Ошибка загрузки файла:', err);
+      res.status(500).json({ message: 'Ошибка загрузки файла' });
+    }
+  });
+
+  // Раздача статики из папки uploads
+app.use('/uploads', express.static('uploads'));
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -29,7 +60,6 @@ app.post('/posts', checkAuth, postValidator, createPost)
 app.delete('/posts/:id', checkAuth, deletePost )
 
 app.patch('/posts/:id',checkAuth, updatePost)
-
 
 
 startServer(app);
